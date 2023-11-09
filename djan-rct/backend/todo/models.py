c4db4746e1c5d15e813d95897f4373cfd486d461
laurencon <laurencontreras25@yahoo.com>
2023-11-09 15:28:29 -0800

    Update models.py

--- djan-rct/backend/todo/models.py
+++ djan-rct/backend/todo/models.py
@@ -1,11 +1,26 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
-
-class Todo(models.Model):
+class Game(models.Model):
     title = models.CharField(max_length=120)
     description = models.TextField()
     completed = models.BooleanField(default=False)
 
     def _str_(self):
         return self.title
+    
+class GameSession(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    game = models.ForeignKey(Game, on_delete=models.CASCADE)
+    startTime = models.DateTimeField(auto_now_add = True)
+    endTime = models.DateTimeField()
+    cookieCount = models.IntegerField(default = 0)
+    autoClickerCount = models.IntegerField(default = 0)
+    farmCount = models.IntegerField(default = 0)
+
+    currentState = models.CharField(max_length = 100)
+    currentStateDate = models.DateTimeField()
+
+    def is_active(self):
+        return self.end_time is None
