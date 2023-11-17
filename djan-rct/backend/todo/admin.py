7891bf5de0add73abc96cd9479263fb9e6b28eb5
laurencon <laurencontreras25@yahoo.com>
2023-11-17 15:35:21 -0800

    Update admin.py

--- djan-rct/backend/todo/admin.py
+++ djan-rct/backend/todo/admin.py
@@ -1,9 +1,13 @@
 from django.contrib import admin
-from .models import Todo
+from .models import ToDo, GameSession
 
 class TodoAdmin(admin.ModelAdmin):
     list_display = ('title', 'description', 'completed')
 
+class GameSessionAdmin(admin.ModelAdmin):
+    list_display = ('user', 'game', 'startTime', 'endTime', 'cookieCount', 'autoClickerCount', 'farmCount')
+
 # Register your models here.
 
 admin.site.register(Todo, TodoAdmin)
+admin.site.register(GameSession, GameSessionAdmin)
