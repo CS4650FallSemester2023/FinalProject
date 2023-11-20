07332838c4e877922a0aa31c48e9f04530af603a
laurencon <laurencontreras25@yahoo.com>
2023-11-20 12:49:52 -0800

    delete game

--- djan-rct/backend/gamesession/models.py
+++ djan-rct/backend/gamesession/models.py
@@ -1,12 +1,10 @@
 from django.db import models
 from django.contrib.auth.models import User
-from todo.models import Todo
 
 
 # Create your models here.
 class GameSession(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    game = models.ForeignKey(Todo, on_delete=models.CASCADE)
     startTime = models.DateTimeField(auto_now_add = True)
     endTime = models.DateTimeField()
     cookieCount = models.IntegerField(default = 0)
