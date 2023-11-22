80a2ff931c6d3945ba23182cbd53b9906fb67faa
SHyguymoll <shymollet@gmail.com>
2023-11-22 20:35:28 +0000

    updated sql model

--- djan-rct/backend/gamesession/admin.py
+++ djan-rct/backend/gamesession/admin.py
@@ -3,6 +3,6 @@ from .models import GameSession
 # Register your models here.
 
 class GameSessionAdmin(admin.ModelAdmin):
-    list_display = ('user', 'startTime', 'endTime', 'cookieCount', 'autoClickerCount')
+    list_display = ('user', 'startTime', 'cookieCount', 'autoClickerCount', 'doubleClickCount')
 
 admin.site.register(GameSession, GameSessionAdmin)
--- djan-rct/backend/gamesession/models.py
+++ djan-rct/backend/gamesession/models.py
@@ -5,12 +5,8 @@ from todo.models import Todo
 
 # Create your models here.
 class GameSession(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.CharField(max_length=100, primary_key=True)
     startTime = models.DateTimeField(auto_now_add = True)
-    endTime = models.DateTimeField()
     cookieCount = models.IntegerField(default = 0)
     autoClickerCount = models.IntegerField(default = 0)
-
-
-    def is_active(self):
-        return self.endTime is None
+    doubleClickCount = models.IntegerField(default = 0)
