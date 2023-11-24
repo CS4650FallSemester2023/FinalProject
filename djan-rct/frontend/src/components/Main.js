0c0598f8d2e6ff411d60fb2c0e6038cf8d95f9c5
SHyguymoll <shymollet@gmail.com>
2023-11-24 21:05:00 +0000

    mass refactoring

--- djan-rct/frontend/src/components/Main.js
+++ djan-rct/frontend/src/components/Main.js
@@ -1,10 +1,11 @@
-import React, {useState, useEffect, useRef} from 'react'
-import Particle from './Particle'
-import Shop from './Shop'
-import cookie from "../images/cookie.png"
-import autoclick from "../images/auto-click.png"
-import x2click from "../images/x2-click.png"
-import axios from 'axios'
+import React, { useState, useEffect, useRef } from 'react';
+import Particle from './Particle';
+import Shop from './Shop';
+import cookie from "../images/cookie.png";
+import autoclick from "../images/auto-click.png";
+import x2click from "../images/x2-click.png";
+import axios from 'axios';
+
 
 export default function Main() {
     // useState hook that uses a state variable to preserve values
@@ -12,38 +13,32 @@ export default function Main() {
     const [particles, setParticles] = useState([]);
     const [autoclickCount, updateAutoClickCount] = useState(0);
     const [x2clickCount, updatex2clickCount] = useState(0);
-    const [startTime, updateStartTime] = useState("")
+    const [startTime, updateStartTime] = useState("");
     const [autoclickPrice, updateAutoClickPrice] = useState(20);
     const [x2clickPrice, updatex2clickPrice] = useState(40);
     const autoclickInterval = useRef(null);
 
     let userid = localStorage.getItem("userid");
     if (!userid) userid = "cs4650";
-    console.log(userid)
+    console.log(userid);
     const [highscoreTable, HSTable] = useState([]);
     function updateHSTable(scoreObject) {
-        HSTable(
-            [
-                ...highscoreTable,
-                {user: scoreObject.userid, cscore: scoreObject.cookieNumber}
-            ]
-        )
+        HSTable([...highscoreTable, { user: scoreObject.userid, cscore: scoreObject.cookieNumber }]);
     }
-    
-    // useCallback to update highscore and save data to server
-    useCallback(() => {
-        loadHighscores();
-        saveData();
-    }, [loadHighscores, saveData])
+    function resetHSTable() {
+        HSTable([])
+    }
 
-    // useCallback to load data from server
-    
-    // useEffect hook to retrieve the counts from local storage after component mounts/page load
-    useEffect(() =>{
-        
+    // useEffect hook to load data and highscore from server on first load
+    useEffect(() => {
         loadData();
+        loadHighscores();
+    }, []);
 
-        for(let i = 0; i < autoclickCount; i++){
+    // useEffect hook to update autoclickers when autoClickCount updates
+    useEffect(() => {
+
+        for (let i = 0; i < autoclickCount; i++) {
             autoClickCookie();
         }
 
@@ -52,9 +47,9 @@ export default function Main() {
         };
     }, [autoclickCount]);
     //re-run everytime autoclickCount changes. Does not accumulate the effect from previous renders and starts new
-
+    
     // function to save current player data to backend
-    function saveData(){
+    function saveData() {
         axios.put(`/api/gamesession/${userid}/`,
             {
                 "startTime": startTime,
@@ -65,73 +60,60 @@ export default function Main() {
                 "doubleClickPrice": x2clickPrice
             })
             .then((res) => {
-                // Save success check
-                if (res.status === 200)
-                    alert("save success!")
-                else
-                    alert("save failed!")
+                alert("save success!");
             })
+            .catch(() => {
+                alert("save failed!");
+            });
     }
 
     // function to load player data from backend, should run when game is loaded
-    function loadData(){
+    function loadData() {
         axios
             .get(`/api/gamesession/${userid}/`)
             .then((res) => {
-                if (res.status !== 200) {
-                    // player data doesn't exist! make a new player
-                    if (res.status == 404) {
+                console.log(res.status);
+                updateStartTime(res.data.startTime);
+                updateCookieCount(parseInt(res.data.cookieCount));
+                updateAutoClickCount(parseInt(res.data.AutoClickCount));
+                updatex2clickCount(parseInt(res.data.doubleClickCount));
+                updateAutoClickPrice(parseInt(res.data.AutoClickPrice));
+                updatex2clickPrice(parseInt(res.data.doubleClickPrice));
+            })
+            .catch((err) => {
+                if (err.response) {
+                    if (err.status === 404)
                         saveData();
-                    }
-                    // something has gone royally wrong
-                    else {
+                    else
                         alert("load failed!");
-                    }
-                }
-                else {
-                    updateStartTime(res.data.startTime);
-                    updateCookieCount(parseInt(res.data.cookieCount));
-                    updateAutoClickCount(parseInt(res.data.AutoClickCount));
-                    updatex2clickCount(parseInt(res.data.doubleClickCount));
-                    updateAutoClickPrice(parseInt(res.data.AutoClickPrice));
-                    updatex2clickPrice(parseInt(res.data.doubleClickPrice));
                 }
             });
     }
 
     // function to get high scores from backend, also runs on first load, might be able to update over time?
     function loadHighscores() {
+        resetHSTable();
         axios
             .get(`api/highscore/`)
-            .then(
-                (res) => {
-                    if (res.status !== 200) {
-                        alert("highscores failed to load!");
-                        
-                    }
-                    else {
-                        let scores = res.data
-                        console.log(scores);
-                        for (let index = 0; index < scores.length; index++) {
-                            updateHSTable(
-                                {
-                                    user: scores[index].user,
-                                    cscore: scores[index].cookieCount
-                                }
-                            );
-                        }
-                    }
+            .then((res) => {
+                let scores = res.data;
+                console.log(scores);
+                for (let index = 0; index < scores.length; index++) {
+                    updateHSTable({ user: scores[index].user, cscore: scores[index].cookieCount });
                 }
-            )
+            })
+            .catch((err) => {
+                alert("highscores failed to load!");
+            });
     }
-    
+
     // function to handle increasing count of cookie when clicked
     function handleCookieCount(e) {
         // update state and trigger a re-render
         updateCookieCount(cookieCount + (2 ** x2clickCount));
 
         // Create a new particle and add it to the particles state
-        const newParticle = {x: e.clientX, y: e.clientY};
+        const newParticle = { x: e.clientX, y: e.clientY };
         setParticles((prevParticles) => [...prevParticles, newParticle]);
     };
 
@@ -141,91 +123,93 @@ export default function Main() {
             updateCookieCount(cookieCount + 1);
         }, 1000);
     }
-      
+
     //function to handle purchasing upgrades
     function handleUpgradePurchase(upgradeName, upgradeCost) {
         // check if the user has enough cookies to purchase the upgrade
         if (cookieCount >= upgradeCost) {
-          // deduct the upgrade cost from the cookie count
-          const newCookieCount = cookieCount - upgradeCost; 
-          updateCookieCount(newCookieCount);
+            // deduct the upgrade cost from the cookie count
+            const newCookieCount = cookieCount - upgradeCost;
+            updateCookieCount(newCookieCount);
 
-          // update 'autoclick' and 'x2clickPrice' price to upgrade and num. of times bought
-          if(upgradeName === 'autoclick'){
-            updateAutoClickCount((prevCount) => prevCount + 1);
-            // autoClickCookie();	
+            // update 'autoclick' and 'x2clickPrice' price to upgrade and num. of times bought
+            if (upgradeName === 'autoclick') {
+                updateAutoClickCount((prevCount) => prevCount + 1);
+                // autoClickCookie();	
+                updateAutoClickPrice((prevPrice) => Math.round(prevPrice * 1.2));
+            }
+            else {
+                updatex2clickCount((prevCount) => prevCount + 1);
 
-            updateAutoClickPrice((prevPrice) => Math.round(prevPrice * 1.2));	
-          }
-          else{
-            updatex2clickCount((prevCount) => prevCount + 1);
-
-            updatex2clickPrice((prevPrice) => Math.round(prevPrice * 2.2));
-          }
-          saveData(userid);
-        } 
+                updatex2clickPrice((prevPrice) => Math.round(prevPrice * 2.2));
+            }
+            saveData(userid);
+        }
         else {
-          alert('Not enough cookies to purchase this upgrade!');
+            alert('Not enough cookies to purchase this upgrade!');
         }
     }
 
     return (
-    <div>   
-        <main className='cookieGame'>
-            {/* Cookie Clicker Play */}
-            <section className='cookiePlay'>
-                <p><span>{cookieCount}</span> cookies</p>
-                <img 
-                    src={cookie} 
-                    className = 'cookieBtn' 
-                    alt="cookie img" 
-                    onClick={handleCookieCount}> 
-                </img>
-            </section>
-            {/* Player Side Bar */}
-            <section className='playerSideBar'>
-                {/* Player Store */}
-                <section className='Store'>
-                    <h2>Store</h2>
-                    <Shop
-                        img = {autoclick}
-                        upgradeName = "Autoclick"
-                        upgradePrice = {`${autoclickPrice} cookies`}
-                        onPurchase={() => handleUpgradePurchase('autoclick', autoclickPrice)}
-                        upgradeCount = {autoclickCount}
-                    />
-                    <Shop
-                        img = {x2click}
-                        upgradeName = "x2 click"
-                        upgradePrice = {`${x2clickPrice} cookies`}
+        <div>
+            <main className='cookieGame'>
+                {/* Cookie Clicker Play */}
+                <section className='cookiePlay'>
+                    <p><span>{cookieCount}</span> cookies</p>
+                    <img
+                        src={cookie}
+                        className='cookieBtn'
+                        alt="cookie img"
+                        onClick={handleCookieCount}>
+                    </img>
+                </section>
+                {/* Player Side Bar */}
+                <section className='playerSideBar'>
+                    {/* Player Store */}
+                    <section className='Store'>
+                        <h2>Store</h2>
+                        <Shop
+                            img={autoclick}
+                            upgradeName="Autoclick"
+                            upgradePrice={`${autoclickPrice} cookies`}
+                            onPurchase={() => handleUpgradePurchase('autoclick', autoclickPrice)}
+                            upgradeCount={autoclickCount} />
+                        <Shop
+                            img={x2click}
+                            upgradeName="x2 click"
+                            upgradePrice={`${x2clickPrice} cookies`}
 
-                        onPurchase={() => handleUpgradePurchase('x2click', x2clickPrice)}
-                        upgradeCount = {x2clickCount}
-                    />
+                            onPurchase={() => handleUpgradePurchase('x2click', x2clickPrice)}
+                            upgradeCount={x2clickCount} />
+                    </section>
+                    {/* Player High Score */}
+                    <section className='HighScore'>
+                        <h2>High Score</h2>
+                        <table>
+                            <thead><tr>
+                                <th>User</th>
+                                <th>Score</th>
+                            </tr></thead>
+                            <tbody>{highscoreTable.map((hs, key) => {
+                                return (
+                                    <tr key={key}>
+                                        <td>{hs.user}</td>
+                                        <td>{hs.cscore}</td>
+                                    </tr>
+                                );
+                            })}</tbody>
+                        </table>
+                    <button type='button' onClick={loadHighscores}>Update Highscore Table</button>
+                    <button type='button' onClick={saveData}>Save Gane</button>
+                    </section>
                 </section>
-                {/* Player High Score */}
-                <section className='HighScore'>
-                    <h2>High Score</h2>
-                    <table>
-                        <tr>
-                            <th>User</th>
-                            <th>Score</th>
-                        </tr>
-                        {highscoreTable.map(hs => (
-                            <tr>
-                                <td>{hs.user}</td>
-                                <td>{hs.cscore}</td>
-                            </tr>
-                        ))}
-                    </table>
-                </section>
-            </section>
-        </main>
-        <div className='cookie-clicks'> {particles.map((particle, index) => (
-            <Particle key={index} x={particle.x} y={particle.y} />
+            </main>
+            <div className='cookie-clicks'> {particles.map((particle, index) => (
+                <Particle key={index} x={particle.x} y={particle.y} />
             ))}
+            </div>
         </div>
-    </div> 
-  )
+    );
 }
+
 
