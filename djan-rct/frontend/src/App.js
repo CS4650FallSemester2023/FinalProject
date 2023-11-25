84b6554fd18c6f3384d2f2e196950cb84533e933
bre-ha20 <bkha@cpp.edu>
2023-11-25 14:58:40 -0800

    Updating the App and style

--- djan-rct/frontend/src/App.js
+++ djan-rct/frontend/src/App.js
@@ -1,6 +1,28 @@
 import React, { Component } from "react";
 import Modal from "./components/Modal";
 import axios from "axios";
+import React, { useState } from "react";
+import logo from './logo.svg';
+import './App.css';
+import { Login } from "./Login";
+import { Register } from "./Register";
+
+function App() {
+  const [currentForm, setCurrentForm] = useState('login');
+
+  const toggleForm = (formName) => {
+    setCurrentForm(formName);
+  }
+
+  return (
+    <div className="App">
+      {
+        currentForm == "login" ? <Login onFormSwitch={toggleForm} /> : <Register onFormSwitch={toggleForm}/>
+      }
+    </div>
+  );
+}
+
 
 
 class App extends Component {
